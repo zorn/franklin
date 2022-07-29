d9f6eb2c958fab45545dd7ccf491913a1c7ac24b
Mike Zornek <zorn@zornlabs.com>
2022-07-29 16:08:48 -0400

    Adds a few default posts.

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,21 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+alias Franklin.CommandedApplication
+alias Franklin.Posts.Commands.CreatePost
+
+# Make some default posts.
+%CreatePost{
+  uuid: Ecto.UUID.generate(),
+  title: "Hello, world one!",
+  published_at: ~U[2022-07-11 13:00:00Z]
+}
+|> CommandedApplication.dispatch()
+
+%CreatePost{
+  uuid: Ecto.UUID.generate(),
+  title: "Hello, world two!",
+  published_at: ~U[2022-07-12 09:00:00Z]
+}
+|> CommandedApplication.dispatch()
