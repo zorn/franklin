640d1be3b4a1442a8c32f9551e919dcefaa28ffd
Mike Zornek <zorn@zornlabs.com>
2022-08-26 15:42:19 -0400

    Clean up spec and docs.

--- lib/franklin/posts/validations.ex
+++ lib/franklin/posts/validations.ex
@@ -1,22 +1,33 @@
 defmodule Franklin.Posts.Validations do
   @moduledoc """
-  Shared types and preconditions for `Post`-related values.
+  Shared validation logic for `Post`-related attributes.
   """
 
   import Ecto.Changeset
 
+  @doc """
+  Accepts and returns a Changeset of a `Post` and applies domain
+  validation rules for the `id` attribute.
+  """
+  @spec validate_id(Ecto.Changeset.t()) :: Ecto.Changeset.t()
   def validate_id(changeset) do
     validate_required(changeset, :id)
   end
 
+  @doc """
+  Accepts and returns a Changeset of a `Post` and applies domain
+  validation rules for the `published_at` attribute.
+  """
   @spec validate_published_at(Ecto.Changeset.t()) :: Ecto.Changeset.t()
   def validate_published_at(changeset) do
     validate_required(changeset, :published_at)
   end
 
   @doc """
-  Sharable validation rules for the title of a Post.
+  Accepts and returns a Changeset of a `Post` and applies domain
+  validation rules for the `title` attribute.
   """
+  @spec validate_title(Ecto.Changeset.t()) :: Ecto.Changeset.t()
   def validate_title(changeset) do
     changeset
     |> validate_required(:title)
