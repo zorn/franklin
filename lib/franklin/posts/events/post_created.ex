febd272a146ad8c344c8a595ae9b98d0ae3510d4
Mike Zornek <zorn@zornlabs.com>
2022-08-26 15:31:38 -0400

    Remove decoding nil since that should never be allowed. If we do need some kind of error catching this is probably not the place to do it.

--- lib/franklin/posts/events/post_created.ex
+++ lib/franklin/posts/events/post_created.ex
@@ -14,10 +14,6 @@ defimpl Commanded.Serialization.JsonDecoder, for: PostCreated do
   Additional decoder logic to convert stringified `published_at`
   DateTime JSON value into an Elixir DateTime value.
   """
-  def decode(%PostCreated{published_at: nil} = event) do
-    event
-  end
-
   def decode(%PostCreated{published_at: published_at} = event) do
     {:ok, datetime, _} = DateTime.from_iso8601(published_at)
     datetime = DateTime.truncate(datetime, :second)
