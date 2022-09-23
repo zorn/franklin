bef09ac2e7e60bae837b47b657f8e3f1af4391a8
Mike Zornek <zorn@zornlabs.com>
2022-09-22 21:10:23 -0400

    Update to Elixir 1.14.0

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Franklin.MixProject do
     [
       app: :franklin,
       version: "0.1.0",
-      elixir: "~> 1.13.4",
+      elixir: "~> 1.14.0",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
