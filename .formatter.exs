8b52213cea456f2a1d7625accd2832e5e7985bb5
Mike Zornek <zorn@zornlabs.com>
2023-01-01 19:57:19 -0500

    Make sure we can format storybook files.

--- .formatter.exs
+++ .formatter.exs
@@ -4,7 +4,7 @@
   inputs: [
     "*.{heex,ex,exs}",
     "priv/*/seeds.exs",
-    "{config,lib,test}/**/*.{heex,ex,exs}"
+    "{config,lib,storybook,test}/**/*.{heex,ex,exs}"
   ],
   subdirectories: ["priv/*/migrations"]
 ]
