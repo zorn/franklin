5325c50cdcb9f8a336debaa802669ca533fb8463
Mike Zornek <zorn@zornlabs.com>
2023-01-03 12:19:56 -0500

    Update error_tag helper to allow for class override.

--- lib/franklin_web/views/error_helpers.ex
+++ lib/franklin_web/views/error_helpers.ex
@@ -8,10 +8,10 @@ defmodule FranklinWeb.ErrorHelpers do
   @doc """
   Generates tag for inlined form input errors.
   """
-  def error_tag(form, field) do
+  def error_tag(form, field, class \\ "invalid-feedback") do
     Enum.map(Keyword.get_values(form.errors, field), fn error ->
       content_tag(:span, translate_error(error),
-        class: "invalid-feedback",
+        class: class,
         phx_feedback_for: input_name(form, field)
       )
     end)
