6cce65456c87a7723fe5d54ac0a3771ec65811b1
Mike Zornek <zorn@zornlabs.com>
2023-02-21 16:15:26 -0500

    Add no pry to iex settings.

--- .iex.exs
+++ .iex.exs
@@ -1,3 +1,9 @@
 alias Franklin.Posts.Commands.CreatePost
 
 alias FranklinWeb.Router.Helpers, as: Routes
+
+# Never pry for `Kernel.dbg()`.
+Application.put_env(:elixir, :dbg_callback, {Macro, :dbg, []})
+
+# Alternative: You can also launch the app with `--no-pry`.
+# $ iex --no-pry -S mix phx.server
