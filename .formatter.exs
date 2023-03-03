6246f91f43d83e330b4dc9ada8cf777e1484aeeb
Mike Zornek <zorn@zornlabs.com>
2023-03-03 17:13:43 -0500

    First pass at some updates.

--- .formatter.exs
+++ .formatter.exs
@@ -1,10 +1,10 @@
 [
-  import_deps: [:ecto, :phoenix],
+  import_deps: [:ecto, :ecto_sql, :phoenix],
   plugins: [Phoenix.LiveView.HTMLFormatter],
   inputs: [
     "*.{heex,ex,exs}",
-    "priv/*/seeds.exs",
-    "{config,lib,storybook,test}/**/*.{heex,ex,exs}"
+    "{config,lib,storybook,test}/**/*.{heex,ex,exs}",
+    "priv/*/seeds.exs"
   ],
   subdirectories: ["priv/*/migrations"]
 ]
---
+++ issue-115-update-phoenix-notes.md
@@ -0,0 +1,1 @@
+I see `{:finch, "~> 0.13"},` in the new template but not sure how it is used.
