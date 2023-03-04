63b706d9a4c94c663697f33f5df63889e5b2207a
Mike Zornek <zorn@zornlabs.com>
2023-03-04 11:06:38 -0500

    Remove unused view dependency.

--- mix.exs
+++ mix.exs
@@ -53,7 +53,6 @@ defmodule Franklin.MixProject do
       {:phoenix_live_dashboard, "~> 0.7.2"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:phoenix_live_view, "~> 0.18.16"},
-      {:phoenix_view, "~> 2.0"},
       {:phoenix, "~> 1.7.1"},
       {:plug_cowboy, "~> 2.5"},
       {:postgrex, ">= 0.0.0"},
