40a981e63ae63bdd84b3dc74ec2f997c02317e06
Mike Zornek <zorn@zornlabs.com>
2023-03-21 12:49:25 -0400

    Apply mix format.

--- mix.exs
+++ mix.exs
@@ -108,7 +108,8 @@ defmodule Franklin.MixProject do
 
       # GitHub-like components to help style the admin.
       # {:primer_live, "~> 0.2.2"}
-      {:primer_live, git: "https://github.com/zorn/primer_live", branch: "zorn-phoenix-html-update"}
+      {:primer_live,
+       git: "https://github.com/zorn/primer_live", branch: "zorn-phoenix-html-update"}
     ]
   end
 
