bf4a3f7da861780e2ac516846a84a49e532279bf
Mike Zornek <zorn@zornlabs.com>
2023-03-22 11:29:04 -0400

    Update links to live navigation.

--- lib/franklin_web/live/admin/articles/index_live.ex
+++ lib/franklin_web/live/admin/articles/index_live.ex
@@ -20,13 +20,13 @@ defmodule FranklinWeb.Admin.Articles.IndexLive do
 
     <.admin_simple_table rows={@articles}>
       <:column :let={article} label="Title">
-        <.link href={~p"/admin/articles/#{article.id}"}><%= article.title %></.link>
+        <.link navigate={~p"/admin/articles/#{article.id}"}><%= article.title %></.link>
       </:column>
       <:column :let={article} label="Published At">
         <%= article.published_at %>
       </:column>
       <:column :let={article} label="Edit">
-        <.link href={~p"/admin/articles/editor/#{article.id}"}>Edit</.link>
+        <.link navigate={~p"/admin/articles/editor/#{article.id}"}>Edit</.link>
       </:column>
     </.admin_simple_table>
     """
