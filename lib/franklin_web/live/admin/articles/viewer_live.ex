887fb9f92a122a4c81c8081acd8f7d1571a52a41
Mike Zornek <zorn@zornlabs.com>
2023-03-29 16:04:19 -0400

    [Fixes #190] Article Editor: Ability to upload and embed images and other assets. (#206)
    * adding notes file
    
    * Update links to live navigation.
    
    * WIP on upload behavior.
    
    * Wrap comment for credo.
    
    * Fix broken test.
    
    * More progress on upload behavior.
    
    * New spelling words.
    
    * New todo item.
    
    * Refactor some design elements from the editor into their own components.
    
    * Remove unused hook.
    
    * Improve inline docs for app.js file.
    
    * Improve docs.
    
    * Add issue link to docs.
    
    * Add inline docs to better explain the purpose of `upload_progress` map.
    
    Convert image urls into markdown image syntax.
    
    * Remove notes.
    
    * Remove debug.
    
    * Move functions so common names and arity are back to back.
    
    * Remove debug.
    
    * Remove puts.
    
    * Remove dbg.

--- lib/franklin_web/live/admin/articles/viewer_live.ex
+++ lib/franklin_web/live/admin/articles/viewer_live.ex
@@ -40,7 +40,7 @@ defmodule FranklinWeb.Admin.Articles.ViewerLive do
     <div class="text-xl font-bold my-4">Article Viewer</div>
 
     <div>
-      <.link href={~p"/admin/articles/editor/#{@article}"}>Edit</.link>
+      <.link navigate={~p"/admin/articles/editor/#{@article}"}>Edit</.link>
     </div>
 
     <h1 id="article-headline" class="text-5xl font-bold my-8"><%= @article.title %></h1>
