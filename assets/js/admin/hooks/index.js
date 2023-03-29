1b417af4b68aedac0576112b8483662b56228023
Mike Zornek <zorn@zornlabs.com>
2023-03-29 14:17:25 -0400

    Remove unused hook.

--- assets/js/admin/hooks/index.js
+++ assets/js/admin/hooks/index.js
@@ -1,8 +1,6 @@
-import AttachmentUrlInserter from './attachment_url_inserter';
 import TextareaMutation from './textarea_mutation';
 
 // https://hexdocs.pm/phoenix_live_view/js-interop.html#client-hooks-via-phx-hook
 let Hooks = {};
-Hooks.AttachmentUrlInserter = AttachmentUrlInserter;
 Hooks.TextareaMutation = TextareaMutation;
 export default Hooks;
