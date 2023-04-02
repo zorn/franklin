6a0a01e75e787eec5850a0def278a536d8ccb08a
Mike Zornek <zorn@zornlabs.com>
2023-04-02 15:19:45 -0400

    Start replacing sign in form with Primer components.

--- lib/franklin_web/live/admin/user_login_live.ex
+++ lib/franklin_web/live/admin/user_login_live.ex
@@ -1,9 +1,32 @@
 defmodule FranklinWeb.Admin.UserLoginLive do
-  use FranklinWeb, :live_view
+  use FranklinWeb, :admin_live_view
 
   def render(assigns) do
     ~H"""
-    <div class="mx-auto max-w-sm">
+    <div class="mt-8 mb-4">
+      <h1 class="text-center font-light">Sign in to Franklin</h1>
+
+      <%!-- FIXME: Not sure why but when I try to customize this border color it never works. --%>
+      <div class="mx-auto w-96">
+        <.admin_flash_messages flash={@flash} />
+
+        <div class="bg-gray-100 border rounded mt-4 px-4 pb-4">
+          <.form
+            :let={f}
+            for={@form}
+            id="login_form"
+            action={~p"/admin/users/log_in"}
+            phx-update="ignore"
+          >
+            <.text_input form={f} field={:email} is_form_group is_full_width />
+            <.text_input form={f} field={:password} is_form_group is_full_width />
+            <.button is_submit is_primary is_full_width>Sign in</.button>
+          </.form>
+        </div>
+      </div>
+    </div>
+
+    <%!-- <div class="mx-auto max-w-sm">
       <.header class="text-center">
         Sign in to account
         <:subtitle>
@@ -31,13 +54,14 @@ defmodule FranklinWeb.Admin.UserLoginLive do
           </.button>
         </:actions>
       </.simple_form>
-    </div>
+    </div> --%>
     """
   end
 
   def mount(_params, _session, socket) do
     email = live_flash(socket.assigns.flash, :email)
     form = to_form(%{"email" => email}, as: "user")
+    dbg(form)
     {:ok, assign(socket, form: form), temporary_assigns: [form: form]}
   end
 end
