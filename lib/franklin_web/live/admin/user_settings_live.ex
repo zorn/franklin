ac65a59b469d6bb924c76a40c2c0334c8c71e497
Mike Zornek <zorn@zornlabs.com>
2023-04-03 16:06:03 -0400

    Removing the ability to change password because I wasn't really fond on the form flow they used, and it was not working for me. For Franklin, it can be added later.

--- lib/franklin_web/live/admin/user_settings_live.ex
+++ lib/franklin_web/live/admin/user_settings_live.ex
@@ -1,8 +1,6 @@
 defmodule FranklinWeb.Admin.UserSettingsLive do
   use FranklinWeb, :admin_live_view
 
-  import FranklinWeb.CoreComponents, only: [input: 1]
-
   alias Franklin.Accounts
 
   def render(assigns) do
@@ -46,90 +44,8 @@ defmodule FranklinWeb.Admin.UserSettingsLive do
             <p>Sold to the highest bidder.</p>
           </:sidebar>
         </.layout>
-
-        <hr />
-
-        <.layout>
-          <:main>
-            <.form
-              :let={f}
-              for={@password_form}
-              id="password_form"
-              action={~p"/admin/users/log_in?_action=password_updated"}
-              method="post"
-              phx-change="validate_password"
-              phx-submit="update_password"
-              phx-trigger-action={@trigger_submit}
-            >
-              <.input
-                field={@password_form[:email]}
-                type="hidden"
-                id="hidden_user_email"
-                value={@current_email}
-              />
-              <.form_group form={f} field={:password} label="New password">
-                <.text_input form={f} field={:password} type="password" />
-              </.form_group>
-              <.form_group form={f} field={:password_confirmation} label="Confirm new password">
-                <.text_input form={f} field={:password_confirmation} type="password" />
-              </.form_group>
-              <.form_group form={f} field={:current_password} label="Current password">
-                <.text_input form={f} field={:current_password} type="password" />
-              </.form_group>
-              <.button is_submit phx-disable-with="Changing...">
-                Change password
-              </.button>
-            </.form>
-          </:main>
-          <:sidebar>
-            <h2>Change Password</h2>
-            <p>Keep it secret. Keep it safe.</p>
-          </:sidebar>
-        </.layout>
       </div>
     </div>
-
-    <%!--
-
-    <div class="space-y-12 divide-y">
-
-      <div>
-        <.simple_form
-          for={@password_form}
-          id="password_form"
-          action={~p"/admin/users/log_in?_action=password_updated"}
-          method="post"
-          phx-change="validate_password"
-          phx-submit="update_password"
-          phx-trigger-action={@trigger_submit}
-        >
-          <.input
-            field={@password_form[:email]}
-            type="hidden"
-            id="hidden_user_email"
-            value={@current_email}
-          />
-          <.input field={@password_form[:password]} type="password" label="New password" required />
-          <.input
-            field={@password_form[:password_confirmation]}
-            type="password"
-            label="Confirm new password"
-          />
-          <.input
-            field={@password_form[:current_password]}
-            name="current_password"
-            type="password"
-            label="Current password"
-            id="current_password_for_password"
-            value={@current_password}
-            required
-          />
-          <:actions>
-            <.button phx-disable-with="Changing...">Change Password</.button>
-          </:actions>
-        </.simple_form>
-      </div>
-    </div> --%>
     """
   end
 
@@ -149,16 +65,11 @@ defmodule FranklinWeb.Admin.UserSettingsLive do
   def mount(_params, _session, socket) do
     user = socket.assigns.current_user
     email_changeset = Accounts.change_user_email(user)
-    password_changeset = Accounts.change_user_password(user)
 
     socket =
       socket
-      |> assign(:current_password, nil)
       |> assign(:email_form_current_password, nil)
-      |> assign(:current_email, user.email)
       |> assign(:email_form, to_form(email_changeset))
-      |> assign(:password_form, to_form(password_changeset))
-      |> assign(:trigger_submit, false)
 
     {:ok, socket}
   end
@@ -196,36 +107,6 @@ defmodule FranklinWeb.Admin.UserSettingsLive do
 
       {:error, changeset} ->
         {:noreply, assign(socket, :email_form, to_form(Map.put(changeset, :action, :insert)))}
-    end
-  end
-
-  def handle_event("validate_password", params, socket) do
-    %{"current_password" => password, "user" => user_params} = params
-
-    password_form =
-      socket.assigns.current_user
-      |> Accounts.change_user_password(user_params)
-      |> Map.put(:action, :validate)
-      |> to_form()
-
-    {:noreply, assign(socket, password_form: password_form, current_password: password)}
-  end
-
-  def handle_event("update_password", params, socket) do
-    %{"current_password" => password, "user" => user_params} = params
-    user = socket.assigns.current_user
-
-    case Accounts.update_user_password(user, password, user_params) do
-      {:ok, user} ->
-        password_form =
-          user
-          |> Accounts.change_user_password(user_params)
-          |> to_form()
-
-        {:noreply, assign(socket, trigger_submit: true, password_form: password_form)}
-
-      {:error, changeset} ->
-        {:noreply, assign(socket, password_form: to_form(changeset))}
     end
   end
 end
