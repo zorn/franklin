fb360eef6372185c549f7cb15ec15e92e67eaee9
Mike Zornek <zorn@zornlabs.com>
2023-04-03 15:42:41 -0400

    WIP on setting page layout updates. Had to adjust how the form changeset worked a little to better align with Primer components.

--- lib/franklin_web/live/admin/user_settings_live.ex
+++ lib/franklin_web/live/admin/user_settings_live.ex
@@ -1,38 +1,98 @@
 defmodule FranklinWeb.Admin.UserSettingsLive do
-  use FranklinWeb, :live_view
+  use FranklinWeb, :admin_live_view
+
+  import FranklinWeb.CoreComponents, only: [input: 1]
 
   alias Franklin.Accounts
 
   def render(assigns) do
     ~H"""
-    <.header class="text-center">
-      Account Settings
-      <:subtitle>Manage your account email address and password settings</:subtitle>
-    </.header>
+    <div class="mt-8 mb-4">
+      <div class="max-w-screen-lg mx-auto">
+        <h1 class="">Account Settings</h1>
+
+        <hr />
+
+        <.layout>
+          <:main>
+            <.admin_flash_messages flash={@flash} />
+
+            <.form
+              :let={f}
+              class="mb-6"
+              for={@email_form}
+              id="email_form"
+              phx-submit="update_email"
+              phx-change="validate_email"
+            >
+              <.text_input form={f} field={:email} type="email" is_form_group />
+
+              <.form_group form={f} field={:current_password} label="Current password">
+                <.text_input
+                  form={f}
+                  field={:current_password}
+                  type="password"
+                  id="current_password_for_password"
+                />
+              </.form_group>
+
+              <.button is_submit phx-disable-with="Changing...">
+                Change email
+              </.button>
+            </.form>
+          </:main>
+          <:sidebar>
+            <h2>Change Email</h2>
+            <p>Sold to the highest bidder.</p>
+          </:sidebar>
+        </.layout>
+
+        <hr />
+
+        <.layout>
+          <:main>
+            <.form
+              :let={f}
+              for={@password_form}
+              id="password_form"
+              action={~p"/admin/users/log_in?_action=password_updated"}
+              method="post"
+              phx-change="validate_password"
+              phx-submit="update_password"
+              phx-trigger-action={@trigger_submit}
+            >
+              <.input
+                field={@password_form[:email]}
+                type="hidden"
+                id="hidden_user_email"
+                value={@current_email}
+              />
+              <.form_group form={f} field={:password} label="New password">
+                <.text_input form={f} field={:password} type="password" />
+              </.form_group>
+              <.form_group form={f} field={:password_confirmation} label="Confirm new password">
+                <.text_input form={f} field={:password_confirmation} type="password" />
+              </.form_group>
+              <.form_group form={f} field={:current_password} label="Current password">
+                <.text_input form={f} field={:current_password} type="password" />
+              </.form_group>
+              <.button is_submit phx-disable-with="Changing...">
+                Change password
+              </.button>
+            </.form>
+          </:main>
+          <:sidebar>
+            <h2>Change Password</h2>
+            <p>Keep it secret. Keep it safe.</p>
+          </:sidebar>
+        </.layout>
+      </div>
+    </div>
+
+    <%!--
 
     <div class="space-y-12 divide-y">
-      <div>
-        <.simple_form
-          for={@email_form}
-          id="email_form"
-          phx-submit="update_email"
-          phx-change="validate_email"
-        >
-          <.input field={@email_form[:email]} type="email" label="Email" required />
-          <.input
-            field={@email_form[:current_password]}
-            name="current_password"
-            id="current_password_for_email"
-            type="password"
-            label="Current password"
-            value={@email_form_current_password}
-            required
-          />
-          <:actions>
-            <.button phx-disable-with="Changing...">Change Email</.button>
-          </:actions>
-        </.simple_form>
-      </div>
+
       <div>
         <.simple_form
           for={@password_form}
@@ -69,7 +129,7 @@ defmodule FranklinWeb.Admin.UserSettingsLive do
           </:actions>
         </.simple_form>
       </div>
-    </div>
+    </div> --%>
     """
   end
 
@@ -104,7 +164,8 @@ defmodule FranklinWeb.Admin.UserSettingsLive do
   end
 
   def handle_event("validate_email", params, socket) do
-    %{"current_password" => password, "user" => user_params} = params
+    %{"user" => user_params} = params
+    %{"current_password" => password} = user_params
 
     email_form =
       socket.assigns.current_user
@@ -116,7 +177,8 @@ defmodule FranklinWeb.Admin.UserSettingsLive do
   end
 
   def handle_event("update_email", params, socket) do
-    %{"current_password" => password, "user" => user_params} = params
+    %{"user" => user_params} = params
+    %{"current_password" => password} = user_params
     user = socket.assigns.current_user
 
     case Accounts.apply_user_email(user, password, user_params) do
@@ -127,7 +189,9 @@ defmodule FranklinWeb.Admin.UserSettingsLive do
           &url(~p"/admin/users/settings/confirm_email/#{&1}")
         )
 
-        info = "A link to confirm your email change has been sent to the new address."
+        info =
+          "Pending change accepted. A link to confirm this email change has been sent to the new address."
+
         {:noreply, socket |> put_flash(:info, info) |> assign(email_form_current_password: nil)}
 
       {:error, changeset} ->
