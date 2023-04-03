ba18485134da8fda189801cdbce684d766e9d79a
Mike Zornek <zorn@zornlabs.com>
2023-04-03 16:26:45 -0400

    Update authentication code paths to admin specific locations.

--- lib/franklin_web/admin/user_auth.ex
+++ lib/franklin_web/admin/user_auth.ex
@@ -81,7 +81,7 @@ defmodule FranklinWeb.Admin.UserAuth do
     conn
     |> renew_session()
     |> delete_resp_cookie(@remember_me_cookie)
-    |> redirect(to: ~p"/")
+    |> redirect(to: ~p"/admin/users/log_in")
   end
 
   @doc """
@@ -223,5 +223,5 @@ defmodule FranklinWeb.Admin.UserAuth do
 
   defp maybe_store_return_to(conn), do: conn
 
-  defp signed_in_path(_conn), do: ~p"/"
+  defp signed_in_path(_conn), do: ~p"/admin"
 end
