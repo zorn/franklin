095e549d02430b77a60c741f0e50b226b255ad48
Mike Zornek <zorn@zornlabs.com>
2023-04-03 16:42:54 -0400

    Fix tests.

--- test/franklin_web/admin/user_auth_test.exs
+++ test/franklin_web/admin/user_auth_test.exs
@@ -22,7 +22,7 @@ defmodule FranklinWeb.Admin.UserAuthTest do
       conn = UserAuth.log_in_user(conn, user)
       assert token = get_session(conn, :user_token)
       assert get_session(conn, :live_socket_id) == "users_sessions:#{Base.url_encode64(token)}"
-      assert redirected_to(conn) == ~p"/"
+      assert redirected_to(conn) == ~p"/admin"
       assert Accounts.get_user_by_session_token(token)
     end
 
@@ -60,7 +60,7 @@ defmodule FranklinWeb.Admin.UserAuthTest do
       refute get_session(conn, :user_token)
       refute conn.cookies[@remember_me_cookie]
       assert %{max_age: 0} = conn.resp_cookies[@remember_me_cookie]
-      assert redirected_to(conn) == ~p"/"
+      assert redirected_to(conn) == ~p"/admin/sign-in"
       refute Accounts.get_user_by_session_token(user_token)
     end
 
@@ -79,7 +79,7 @@ defmodule FranklinWeb.Admin.UserAuthTest do
       conn = conn |> fetch_cookies() |> UserAuth.log_out_user()
       refute get_session(conn, :user_token)
       assert %{max_age: 0} = conn.resp_cookies[@remember_me_cookie]
-      assert redirected_to(conn) == ~p"/"
+      assert redirected_to(conn) == ~p"/admin/sign-in"
     end
   end
 
@@ -216,7 +216,7 @@ defmodule FranklinWeb.Admin.UserAuthTest do
     test "redirects if user is authenticated", %{conn: conn, user: user} do
       conn = conn |> assign(:current_user, user) |> UserAuth.redirect_if_user_is_authenticated([])
       assert conn.halted
-      assert redirected_to(conn) == ~p"/"
+      assert redirected_to(conn) == ~p"/admin"
     end
 
     test "does not redirect if user is not authenticated", %{conn: conn} do
