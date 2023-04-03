fec54adea6daf011a3389d3f271bb7e92d4957e2
Mike Zornek <zorn@zornlabs.com>
2023-04-03 16:52:37 -0400

    Fix tests, remove password form tests.

--- test/franklin_web/live/admin/user_settings_live_test.exs
+++ test/franklin_web/live/admin/user_settings_live_test.exs
@@ -13,7 +13,6 @@ defmodule FranklinWeb.Admin.UserSettingsLiveTest do
         |> live(~p"/admin/users/settings")
 
       assert html =~ "Change Email"
-      assert html =~ "Change Password"
     end
 
     test "redirects if user is not logged in", %{conn: conn} do
@@ -40,12 +39,13 @@ defmodule FranklinWeb.Admin.UserSettingsLiveTest do
       result =
         lv
         |> form("#email_form", %{
-          "current_password" => password,
-          "user" => %{"email" => new_email}
+          "user" => %{"current_password" => password, "email" => new_email}
         })
         |> render_submit()
 
-      assert result =~ "A link to confirm your email"
+      assert result =~
+               "Pending change accepted. A link to confirm this email change has been sent to the new address."
+
       assert Accounts.get_user_by_email(user.email)
     end
 
@@ -71,89 +71,12 @@ defmodule FranklinWeb.Admin.UserSettingsLiveTest do
       result =
         lv
         |> form("#email_form", %{
-          "current_password" => "invalid",
-          "user" => %{"email" => user.email}
+          "user" => %{"current_password" => "invalid", "email" => user.email}
         })
         |> render_submit()
 
       assert result =~ "Change Email"
       assert result =~ "did not change"
-      assert result =~ "is not valid"
-    end
-  end
-
-  describe "update password form" do
-    setup %{conn: conn} do
-      password = valid_user_password()
-      user = user_fixture(%{password: password})
-      %{conn: log_in_user(conn, user), user: user, password: password}
-    end
-
-    test "updates the user password", %{conn: conn, user: user, password: password} do
-      new_password = valid_user_password()
-
-      {:ok, lv, _html} = live(conn, ~p"/admin/users/settings")
-
-      form =
-        form(lv, "#password_form", %{
-          "current_password" => password,
-          "user" => %{
-            "email" => user.email,
-            "password" => new_password,
-            "password_confirmation" => new_password
-          }
-        })
-
-      render_submit(form)
-
-      new_password_conn = follow_trigger_action(form, conn)
-
-      assert redirected_to(new_password_conn) == ~p"/admin/users/settings"
-
-      assert get_session(new_password_conn, :user_token) != get_session(conn, :user_token)
-
-      assert Phoenix.Flash.get(new_password_conn.assigns.flash, :info) =~
-               "Password updated successfully"
-
-      assert Accounts.get_user_by_email_and_password(user.email, new_password)
-    end
-
-    test "renders errors with invalid data (phx-change)", %{conn: conn} do
-      {:ok, lv, _html} = live(conn, ~p"/admin/users/settings")
-
-      result =
-        lv
-        |> element("#password_form")
-        |> render_change(%{
-          "current_password" => "invalid",
-          "user" => %{
-            "password" => "too short",
-            "password_confirmation" => "does not match"
-          }
-        })
-
-      assert result =~ "Change Password"
-      assert result =~ "should be at least 12 character(s)"
-      assert result =~ "does not match password"
-    end
-
-    test "renders errors with invalid data (phx-submit)", %{conn: conn} do
-      {:ok, lv, _html} = live(conn, ~p"/admin/users/settings")
-
-      result =
-        lv
-        |> form("#password_form", %{
-          "current_password" => "invalid",
-          "user" => %{
-            "password" => "too short",
-            "password_confirmation" => "does not match"
-          }
-        })
-        |> render_submit()
-
-      assert result =~ "Change Password"
-      assert result =~ "should be at least 12 character(s)"
-      assert result =~ "does not match password"
       assert result =~ "is not valid"
     end
   end
