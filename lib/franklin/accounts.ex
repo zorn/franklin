1cf0e4c7339ff7c216138a5207bfdbdda8238a93
Mike Zornek <zorn@zornlabs.com>
2023-04-25 17:12:38 -0400

    Add an alternative non-bang version of `get_user/1`.

--- lib/franklin/accounts.ex
+++ lib/franklin/accounts.ex
@@ -63,6 +63,21 @@ defmodule Franklin.Accounts do
   """
   def get_user!(id), do: Repo.get!(User, id)
 
+  @doc """
+  Gets a single user.
+
+  Returns `nil` if the User does not exist.
+
+  ## Examples
+
+      iex> get_user(123)
+      %User{}
+
+      iex> get_user(456)
+      nil
+  """
+  def get_user(id), do: Repo.get(User, id)
+
   ## User registration
 
   @doc """
